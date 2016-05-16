071704037f0225dfccec4db7708d1c0a63aca5ed
nmadd <nate@fratmusic.com>
2016-05-16 16:04:58 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Build out the Express portion of the app!
+
+//Step 1: Import in express using the 'require' keyword
+
+//Step 2: Create routes to send back the correct view for each url (remember to use '__dirname' in front of your file path)
+
+//Step 3: configure the app to listen on port 3000
+
+//BONUS 1: Add additional themes, and a new routes and views for each new theme
+
+//BONUS 2: Require in the chalk package and console.log a different color message for each route
+
+//BONUS 3: Keep improving on the app and make it your own!
